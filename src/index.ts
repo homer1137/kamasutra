3b4aa8c6fbbdca273535618eec29038391264837
Romango <homer37@yandex.ru>
2023-06-20 11:26:30 +0400

    start of the project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express"
+
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+
+  res.send('Hello World22233!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port 2 ${port}`)
+})
